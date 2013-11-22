ce678e8bcd2043ae90dc625e1d2e488fe6c19824
careyna <alquimist118@hotmail.com>
2013-11-21 19:51:05 -0500

    Creacion del Test Rol

---
+++ src/test/java/clickandbuy/upc/edu/core/business/RollBusinessTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clickandbuy.upc.edu.core.business;
+
+import clickandbuy.upc.edu.core.entity.Rol;
+import clickandbuy.upc.edu.core.entity.Usuario;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+/**
+ *
+ * @author carlos
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class RollBusinessTest {
+public RollBusinessTest(){
+}
+     
+}
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+
+    
+    
