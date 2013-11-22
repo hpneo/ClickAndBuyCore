3f9ae0dc6af640b19bbf05517fdb26b69c9f66b5
cpuellesc <christian.puelles.c@gmail.com>
2013-11-21 20:02:16 -0500

    Creacion de Test

--- src/test/java/clickandbuy/upc/edu/core/business/CategoriaBusinessTest.java
+++ src/test/java/clickandbuy/upc/edu/core/business/CategoriaBusinessTest.java
@@ -1,13 +1,26 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package clickandbuy.upc.edu.core.business;
+
+import junit.framework.TestCase;
 
 /**
  *
- * @author Lenovo
+ * @author cpuellesc
  */
-public class CategoriaBusinessTest {
+public class CategoriaBusinessTest extends TestCase{
     
+    public CategoriaBusinessTest(String testName){
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+    
+    public void
 }
