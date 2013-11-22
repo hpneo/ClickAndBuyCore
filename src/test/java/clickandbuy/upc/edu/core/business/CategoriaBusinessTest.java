7f4ed863e738335c287f90a58f4a906ba30733af
cpuellesc <christian.puelles.c@gmail.com>
2013-11-21 19:48:16 -0500

    Creación de Test para Categoría

---
+++ src/test/java/clickandbuy/upc/edu/core/business/CategoriaBusinessTest.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clickandbuy.upc.edu.core.business;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class CategoriaBusinessTest {
+    
+}
