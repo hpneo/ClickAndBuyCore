4882ba8c0155403d534ee5062834106385d0606c
cpuellesc <christian.puelles.c@gmail.com>
2013-11-21 20:28:44 -0500

    Update de CategoriaDAO

--- src/main/java/clickandbuy/upc/edu/core/dao/CategoriaDAO.java
+++ src/main/java/clickandbuy/upc/edu/core/dao/CategoriaDAO.java
@@ -1,21 +1,15 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package clickandbuy.upc.edu.core.dao;
 
 import clickandbuy.upc.edu.core.entity.Categoria;
 import java.util.List;
 
-/**
- *
- * @author Garyfimo
- */
 public interface CategoriaDAO {
 
     public void addCategoria(Categoria categoria) throws Exception;
 
     public Categoria getCategoria(Integer cat_codigo) throws Exception;
-
+    
+    public Categoria getCategoriaXNombre(String nombre) throws Exception;
+    
     public List<Categoria> listCategoria() throws Exception;
 }
