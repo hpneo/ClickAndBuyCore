6349f281ba750912a7d0e000da0250e5e2a9d751
Gary Figuerola Mora <garyfimo@gmail.com>
2013-11-23 18:56:33 -0500

    Cliente

---
+++ src/main/java/clickandbuy/upc/edu/core/exception/ClienteException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clickandbuy.upc.edu.core.exception;
+
+/**
+ *
+ * @author Garyfimo
+ */
+public class ClienteException extends ClickandbuyException {
+
+    public ClienteException(String s) {
+        super(s);
+    }
+}
