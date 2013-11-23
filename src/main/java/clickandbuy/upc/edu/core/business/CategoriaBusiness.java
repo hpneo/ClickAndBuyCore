24a31f9fcc5b7902d8447b75e27c00e76015eb04
HPNeo <hpneo@hotmail.com>
2013-11-23 18:50:54 -0500

    Fix conflicts

--- src/main/java/clickandbuy/upc/edu/core/business/CategoriaBusiness.java
+++ src/main/java/clickandbuy/upc/edu/core/business/CategoriaBusiness.java
@@ -2,6 +2,7 @@ package clickandbuy.upc.edu.core.business;
 
 import clickandbuy.upc.edu.core.dao.CategoriaDAO;
 import clickandbuy.upc.edu.core.entity.Categoria;
+import clickandbuy.upc.edu.core.exception.CategoriaException;
 import clickandbuy.upc.edu.core.impl.CategoriaImpl;
 import java.util.List;
 
@@ -9,19 +10,19 @@ public class CategoriaBusiness {
 
     private CategoriaDAO categoriadao = new CategoriaImpl();
 
-    public void addCategoria(Categoria categoria) throws Exception {
+    public void addCategoria(Categoria categoria) throws CategoriaException {
         categoriadao.addCategoria(categoria);
     }
 
-    public Categoria getCategoriaByCode(Integer catCodigo) throws Exception {
+    public Categoria getCategoriaByCode(Integer catCodigo) throws CategoriaException {
         return categoriadao.getCategoria(catCodigo);
     }
 
-    public Categoria getCategoriaXNombre(String nombre) throws Exception {
+    public Categoria getCategoriaXNombre(String nombre) throws CategoriaException {
         return categoriadao.getCategoriaXNombre(nombre);
     }
 
-    public List<Categoria> listCategoria() throws Exception {
+    public List<Categoria> listCategoria() throws CategoriaException {
         return categoriadao.listCategoria();
     }
 }
--- src/main/java/clickandbuy/upc/edu/core/dao/CategoriaDAO.java
+++ src/main/java/clickandbuy/upc/edu/core/dao/CategoriaDAO.java
@@ -1,15 +1,16 @@
 package clickandbuy.upc.edu.core.dao;
 
 import clickandbuy.upc.edu.core.entity.Categoria;
+import clickandbuy.upc.edu.core.exception.CategoriaException;
 import java.util.List;
 
 public interface CategoriaDAO {
 
-    public void addCategoria(Categoria categoria) throws Exception;
+    public void addCategoria(Categoria categoria) throws CategoriaException;
 
-    public Categoria getCategoria(Integer catCodigo) throws Exception;
+    public Categoria getCategoria(Integer catCodigo) throws CategoriaException;
 
-    public Categoria getCategoriaXNombre(String nombre) throws Exception;
+    public Categoria getCategoriaXNombre(String nombre) throws CategoriaException;
 
-    public List<Categoria> listCategoria() throws Exception;
+    public List<Categoria> listCategoria() throws CategoriaException;
 }
