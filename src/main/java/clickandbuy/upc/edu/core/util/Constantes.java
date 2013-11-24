4b339a6639bf614e23c24d4004be6207f5867891
Gary Figuerola Mora <garyfimo@gmail.com>
2013-11-23 20:23:10 -0500

    lkxfn

--- src/main/java/clickandbuy/upc/edu/core/util/Constantes.java
+++ src/main/java/clickandbuy/upc/edu/core/util/Constantes.java
@@ -13,6 +13,46 @@ public class Constantes {
     private Constantes() {
     }
 
+    public static String getERROR_CONEXION() {
+        return ERROR_CONEXION;
+    }
+
+    public static String getERROR_ID() {
+        return ERROR_ID;
+    }
+
+    public static String getERROR_QUERY() {
+        return ERROR_QUERY;
+    }
+
+    public static String getERROR_MAPEO() {
+        return ERROR_MAPEO;
+    }
+
+    public static String getERROR_HIBERNATE() {
+        return ERROR_HIBERNATE;
+    }
+
+    public static int getCARACTERES_NOMBREUSUARIO() {
+        return CARACTERES_NOMBREUSUARIO;
+    }
+
+    public static int getCARACTERES_CLIENTE() {
+        return CARACTERES_CLIENTE;
+    }
+
+    public static int getCARACTERES_CONTRASENA() {
+        return CARACTERES_CONTRASENA;
+    }
+
+    public static String getCODIGO() {
+        return CODIGO;
+    }
+
+    public static String getSELECT_FROM_PEDIDO_WHERE() {
+        return SELECT_FROM_PEDIDO_WHERE;
+    }
+
     private static final String ERROR_CONEXION = "No se pudo establecer conexión con la base de datos.";
     private static final String ERROR_ID = "Error al generar ID en la base de datos";
     private static final String ERROR_QUERY = "Query inválido.";
